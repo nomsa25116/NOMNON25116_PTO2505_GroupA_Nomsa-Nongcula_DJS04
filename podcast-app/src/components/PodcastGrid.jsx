1ebbdfbe01c72839b759bfa7a0f43f0f620e0b33
nomsa25116 <ntnongcula@gmail.com>
2026-03-16 00:29:03 +0200

    updated PodcastGrid

--- podcast-app/src/components/PodcastGrid.jsx
+++ podcast-app/src/components/PodcastGrid.jsx
@@ -1,12 +1,15 @@
-import PodcastCard from "./PodcastCard";
+import { useContext } from 'react';
+import { PodcastCard } from './PodcastCard';
+import { PodcastContext } from '../context/PodcastContext';
 
+export default function PodcastGrid( genres) {
+    const { podcasts } = useContext(PodcastContext);
 
-export default function PodcastGrid({ podcasts, genres }) {
-  return (
-    <div className="grid">
-      {podcasts.map((podcast) => (
-        <PodcastCard key={podcast.id} podcast={podcast} genres={genres} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="grid">
+            {podcasts.map((podcast) => (
+                <PodcastCard key={podcast.id} podcast={podcast} genres={genres} />
+            ))}
+        </div>
+    );
 }
