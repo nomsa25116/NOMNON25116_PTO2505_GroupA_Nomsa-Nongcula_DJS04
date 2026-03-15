8ecf59935f464eded7b948746eeeda762307fbf1
nomsa25116 <ntnongcula@gmail.com>
2026-03-16 00:07:17 +0200

    Add Pagination.jsx

---
+++ podcast-app/src/components/Pigination.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+import { PodcastContext } from "../context/PodcastContext";
+
+/**
+ * Numeric pigination
+ */
+
+export default function Pigination() {
+    const { page, setPage, totalPages } = useContext(PodcastContext);
+
+    if (totalPages <= 1) return null;
+
+    /**Build page */
+const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+
+return (
+    <div className={styles.piginationWrapper}>
+        {pages.map((p) => (
+            <button
+                key={p}
+                className={`${styles.pageButton} ${p === page ? styles.active : ""}`}
+                onClick={() => setPage(p)}
+            >
+                {p}
+            </button>
+        ))}
+    </div>
+);
+}
